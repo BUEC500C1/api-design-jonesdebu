edfa0397e77ce5c3d0543a700643851d30e7fa0d
jonesdebu <33497013+jonesdebu@users.noreply.github.com>
2020-01-29 17:53:38 -0500

    added some functionality
    user input to text box and python function to return the inputed variable.

--- twitter_api.py
+++ twitter_api.py
@@ -1,13 +1,23 @@
 import tweepy
-from flask import Flask
-
+from flask import Flask, render_template, request
+# User passes twitter handle handle
+# Function returns text using google vision
+#get link of media and save it for the google vision API
 app = Flask(__name__)
 
 @app.route("/")
 
 def home():
+    return render_template("home.html")
 
-    return "Hello World!"
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/", methods=['POST'])
+def get_user_input():
+    variable = request.form['variable']
+    return variable
 
 if __name__ == "__main__":
     app.run(debug=True)
