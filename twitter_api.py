ad111ea1031ca8fa30e9081b8ae2c9b28a1a965a
jonesdebu <33497013+jonesdebu@users.noreply.github.com>
2020-01-29 19:41:52 -0500

    updated source code
    Got Tweepy working with python code, can get tweets in text for user with access token that is signed in currently jonesde (me). I also have Flask running for display if needed/ if there is tie to implement some more features.

--- twitter_api.py
+++ twitter_api.py
@@ -5,6 +5,20 @@ from flask import Flask, render_template, request
 # get link of media and save it for the google vision API
 # Function hadling for twitter api and google vision api need to be in a Function
 # function must be called from a different functions
+
+#twitter API OAuth 2 AUthentication
+auth = tweepy.OAuthHandler("4IVCCtuSQ3eLb8i2kH70lVawI", "7erpsGo0gNJTPnTEYc73PFkB6iCvJZrK8HIb7vVm45ywKfZ04d")
+auth.set_access_token("1222366331825090560-pTDch0qvoCkSvFVdgUoybz8OMQy5c8", "7b5LVDnfykKS5Xi5YYH4uiPAZNovzG2HnrpLE2OXV0whZ")
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+
+for tweet in public_tweets:
+    print(tweet.text)
+
+print("Before Flask start")
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -22,10 +36,14 @@ def about():
 # this is supposed to be a library that you can import so the
 # user of this library should be entering the twitter handle
 @app.route("/", methods=['POST'])
-def get_user_input():
+def get_user_input():#(twitter_handle)
     # user_name = input("Enter twitter handle:")
     variable = request.form['variable']
     return variable
+
+#@app.route("/")
+#def get_feed(twitter_handle):
+#    user =
 
 if __name__ == "__main__":
     app.run(debug=True)
