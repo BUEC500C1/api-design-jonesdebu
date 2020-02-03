81201ff491daa278b7e5c3b330f299a8ff8230af
jonesdebu <33497013+jonesdebu@users.noreply.github.com>
2020-02-03 18:23:43 -0500

    Got google vision and tweepy working

--- test_api.py
+++ test_api.py
@@ -3,5 +3,5 @@ from twitter_api import read_tweets
 def test_placeholder():
     pass
 
-def test_read_tweets():
-    assert read_tweets() == "Test"
+#def test_read_tweets():
+    #assert read_tweets() == "Test"
--- twitter_api.py
+++ twitter_api.py
@@ -1,5 +1,5 @@
 import tweepy
-from flask import Flask, render_template, request
+from google.cloud import vision
 # User passes twitter handle handle
 # Function returns text using google vision
 # get link of media and save it for the google vision API
@@ -7,45 +7,30 @@ from flask import Flask, render_template, request
 # function must be called from a different functions
 
 
-def read_tweets():
-    #twitter API OAuth 2 AUthentication
-    auth = tweepy.OAuthHandler("4IVCCtuSQ3eLb8i2kH70lVawI", "7erpsGo0gNJTPnTEYc73PFkB6iCvJZrK8HIb7vVm45ywKfZ04d")
-    auth.set_access_token("1222366331825090560-pTDch0qvoCkSvFVdgUoybz8OMQy5c8", "7b5LVDnfykKS5Xi5YYH4uiPAZNovzG2HnrpLE2OXV0whZ")
+#def read_tweets():
+#twitter API OAuth 2 AUthentication
+auth = tweepy.OAuthHandler("4IVCCtuSQ3eLb8i2kH70lVawI", "7erpsGo0gNJTPnTEYc73PFkB6iCvJZrK8HIb7vVm45ywKfZ04d")
+auth.set_access_token("1222366331825090560-pTDch0qvoCkSvFVdgUoybz8OMQy5c8", "7b5LVDnfykKS5Xi5YYH4uiPAZNovzG2HnrpLE2OXV0whZ")
 
-    api = tweepy.API(auth)
+api = tweepy.API(auth)
 
-    public_tweets = api.home_timeline()
+#tweepy.Cursor(api.search, q="hashtag", count=5, include_entities=True)
 
-    for tweet in public_tweets:
-        print(tweet.text)
+#public_tweets = api.home_timeline()
 
-#print("Before Flask start")
+#get user timeline for tweets to convert
+user = api.get_user('Donovan01060515')
+tweets = api.user_timeline(user.screen_name)
 
-#app = Flask(__name__)
-
-#@app.route("/")
-
-#def home():
-    #return render_template("home.html")
-
-#@app.route("/about")
-#def about():
-    #return render_template("about.html")
+#setup google vision API
+client = vision.ImageAnnotatorClient()
+image = vision.types.Image()
 
 
-# User input needs to be from function call and not html page
-# Just pass whatever twitter handle to this function because
-# this is supposed to be a library that you can import so the
-# user of this library should be entering the twitter handle
-#@app.route("/", methods=['POST'])
-#def get_user_input():#(twitter_handle)
-    # user_name = input("Enter twitter handle:")
-    #variable = request.form['variable']
-    #return variable
+image.source.image_uri = 'gs://cloud-samples-data/vision/using_curl/shanghai.jpeg'# images['media_url']
+response = client.label_detection(image=image)
+for label in response.label_annotations:
+    print(label.description)
 
-#@app.route("/")
-#def get_feed(twitter_handle):
-#    user =
-
-#if __name__ == "__main__":
-    #app.run(debug=True)
+for tweet in tweets:
+    print(tweet.text)
