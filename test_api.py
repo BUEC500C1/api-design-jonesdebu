8bb3cebafff5e5ba75e1cbd9e6bf73450b92347c
jonesdebu <33497013+jonesdebu@users.noreply.github.com>
2020-02-03 16:20:26 -0500

    testing function with pytest

--- test_api.py
+++ test_api.py
@@ -1,3 +1,7 @@
 import pytest
+from twitter_api import read_tweets
 def test_placeholder():
     pass
+
+def test_read_tweets():
+    assert read_tweets == "test"
--- twitter_api.py
+++ twitter_api.py
@@ -6,44 +6,46 @@ from flask import Flask, render_template, request
 # Function hadling for twitter api and google vision api need to be in a Function
 # function must be called from a different functions
 
-#twitter API OAuth 2 AUthentication
-auth = tweepy.OAuthHandler("4IVCCtuSQ3eLb8i2kH70lVawI", "7erpsGo0gNJTPnTEYc73PFkB6iCvJZrK8HIb7vVm45ywKfZ04d")
-auth.set_access_token("1222366331825090560-pTDch0qvoCkSvFVdgUoybz8OMQy5c8", "7b5LVDnfykKS5Xi5YYH4uiPAZNovzG2HnrpLE2OXV0whZ")
 
-api = tweepy.API(auth)
+def read_tweets():
+    #twitter API OAuth 2 AUthentication
+    auth = tweepy.OAuthHandler("4IVCCtuSQ3eLb8i2kH70lVawI", "7erpsGo0gNJTPnTEYc73PFkB6iCvJZrK8HIb7vVm45ywKfZ04d")
+    auth.set_access_token("1222366331825090560-pTDch0qvoCkSvFVdgUoybz8OMQy5c8", "7b5LVDnfykKS5Xi5YYH4uiPAZNovzG2HnrpLE2OXV0whZ")
 
-public_tweets = api.home_timeline()
+    api = tweepy.API(auth)
 
-for tweet in public_tweets:
-    print(tweet.text)
+    public_tweets = api.home_timeline()
 
-print("Before Flask start")
+    for tweet in public_tweets:
+        print(tweet.text)
 
-app = Flask(__name__)
+#print("Before Flask start")
 
-@app.route("/")
+#app = Flask(__name__)
 
-def home():
-    return render_template("home.html")
+#@app.route("/")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+#def home():
+    #return render_template("home.html")
+
+#@app.route("/about")
+#def about():
+    #return render_template("about.html")
 
 
 # User input needs to be from function call and not html page
 # Just pass whatever twitter handle to this function because
 # this is supposed to be a library that you can import so the
 # user of this library should be entering the twitter handle
-@app.route("/", methods=['POST'])
-def get_user_input():#(twitter_handle)
+#@app.route("/", methods=['POST'])
+#def get_user_input():#(twitter_handle)
     # user_name = input("Enter twitter handle:")
-    variable = request.form['variable']
-    return variable
+    #variable = request.form['variable']
+    #return variable
 
 #@app.route("/")
 #def get_feed(twitter_handle):
 #    user =
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+    #app.run(debug=True)
